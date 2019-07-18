46047f1d1f440f5260ad137e7bbb38e3def2ca05
Sawyer Nye <sawyer.nye@live.com>
2019-07-18 13:46:15 -0400

    commit progress to app; implemented games component as well as game streams component

---
+++ src/api.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const api = axios.create({
+  headers: {
+    'Client-ID': '1eyo0tyx5j86iyd16sgxvm81y4g41v'
+  }
+});
+
+export default api;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Games from './components/Games';
+import Header from './components/Header';
+import Streams from './components/Streams';
+import GameStreams from './components/GameStreams';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import './styles.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'shards-ui/dist/css/shards.min.css';
+
+const App = () => {
+
+  return (
+    <Router>
+      <Header />
+      <Route exact path='/' component={Games} />
+      <Route exact path='/top-streams' component={Streams} />
+      <Route exact path='/game/:id' component={GameStreams} />
+    </Router>
+  );
+}
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(
+  <App />,
+  rootElement
+);
